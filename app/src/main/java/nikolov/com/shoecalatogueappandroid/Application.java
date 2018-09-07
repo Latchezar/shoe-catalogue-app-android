65c625b3c4e0194beaed43db51a04f470a85f649
Latchezar Nikolov <latcho.nikolov@gmail.com>
2018-09-07 19:25:28 +0300

    some dagger cahnges

--- app/src/main/java/nikolov/com/shoecalatogueappandroid/Application.java
+++ app/src/main/java/nikolov/com/shoecalatogueappandroid/Application.java
@@ -2,12 +2,12 @@ package nikolov.com.shoecalatogueappandroid;
 
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
+import nikolov.com.shoecalatogueappandroid.diconfig.DaggerAppComponent;
 
 public class Application extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return null;
-
         //return DaggerAppComponent.builder().application(this).build();
     }
 }
