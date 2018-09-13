c8c0495247505fb6cf18c917d92d783df2901feb
Latchezar Nikolov <latcho.nikolov@gmail.com>
2018-09-13 10:51:40 +0300

    small changes on dagger and product list package

---
+++ app/src/main/java/nikolov/com/shoecalatogueappandroid/views/productList/ProductListAdapter.java
@@ -0,0 +1,35 @@
+package nikolov.com.shoecalatogueappandroid.views.productList;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import javax.inject.Inject;
+
+import nikolov.com.shoecalatogueappandroid.R;
+import nikolov.com.shoecalatogueappandroid.models.Product;
+
+public class ProductListAdapter extends ArrayAdapter<Product> {
+    @Inject
+    public ProductListAdapter(@NonNull Context context) {
+        super(context, -1);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View converView, @NonNull ViewGroup parent){
+        LayoutInflater layoutInflater =
+                (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = layoutInflater.inflate(
+                R.layout.product_item,
+                parent,
+                false
+        );
+
+    }
+}
